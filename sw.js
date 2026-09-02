0307d2d4d5a8dbdb3ddffc76f5afdda92cd3428d
yuriilosiev-png <yuriilosiev@gmail.com>
2026-09-02 07:08:03 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
    Версию CACHE всё равно меняем при релизе, чтобы очистить старый кэш иконок. */
 
-const CACHE = 'planner-v3-9a48519a';
+const CACHE = 'planner-v4-1a7b2c';
 const ASSETS = [
   './manifest.json',
   './icon-192.png',
